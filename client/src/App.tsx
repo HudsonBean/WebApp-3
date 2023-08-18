833f3b273ef7cc080dc51a734bec22e56a150a5c
Hudson <hudsonbeandev@gmail.com>
2023-08-18 11:07:00 -0500

    created hooks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={}>
+    <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path="" Component={} />
