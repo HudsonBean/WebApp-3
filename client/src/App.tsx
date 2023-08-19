98d974d7d24395931b83487234bdf05522795bf3
Hudson <hudsonbeandev@gmail.com>
2023-08-19 17:51:34 -0500

    idk weekend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store } from "./store";
+import { Home } from "./Pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="" Component={} />
+          <Route path="" Component={Home} />
         </Routes>
       </BrowserRouter>
     </Provider>
